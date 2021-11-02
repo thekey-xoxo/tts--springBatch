29a914df040261cd48cc997311c71a6f3bcb6ab4
thekey <thekey@hellonature.net>
2021-11-02 18:10:48 +0900

    initial commit
    commit

---
+++ src/main/java/com/constkey/stdspringbatch/StdSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.constkey.stdspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/constkey/stdspringbatch/StdSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.constkey.stdspringbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
